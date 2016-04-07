cd48286375133d448480eecbe9068d395b52a9d3
Sergej Kasper <sergejkasper@gmail.com>
2016-04-07 03:13:37 +0200

    basic tests and decorator prototype

--- test/unit/App.spec.ts
+++ test/unit/App.spec.ts
@@ -1,11 +1,28 @@
 ///// <reference path="../references.ts" />
 import {App} from "../../examples/App";
+import {ngIOC} from "../../src/ngIOC";
+import {HeaderDirective} from "../../examples/components/header/Header.controller";
 
 describe("App", () => {
   describe("during initialization", () => {
     it("is alive", () => {
-      //App.should.not.be.undefined;
       chai.assert(typeof App !== undefined)
+    });
+    it("is has injector", () => {
+        chai.assert(ngIOC.defaultModule === angular.module("app"))
     });
   });
 });
+/*describe("Directive", () => {
+  beforeEach(inject(function($compile, $rootScope){
+    $compile('<div header="{{is Loaded}}"></div>', $rootScope.$new());
+  }))
+  describe("during initialization", () => {
+    it("is alive", () => {
+      chai.assert(typeof HeaderDirective !== undefined)
+    });
+    //it("is has injector", () => {
+    //    chai.assert(HeaderDirective.isLoaded);
+    //});
+  });
+});*/
